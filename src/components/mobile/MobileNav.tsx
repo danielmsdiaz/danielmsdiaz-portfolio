329807744f7d0e3dd9393bbda7dc9a16d135ac7c
Daniel <dmachadosdiaz@gmail.com>
2025-01-29 15:10:40 -0300

    fix

--- src/components/mobile/MobileNav.tsx
+++ src/components/mobile/MobileNav.tsx
@@ -3,7 +3,6 @@ import { AlignJustify } from "lucide-react"
 import Nav from "../Nav"
 import Logo from "../Logo"
 import Socials from "../Socials"
-
 import { useState } from "react"
 
 const MobileNav = () => {
@@ -18,7 +17,7 @@ const MobileNav = () => {
       <SheetTrigger asChild>
         <AlignJustify cursor="pointer" />
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent> 
         <div className="flex flex-col items-center justify-between h-full py-3">
           <div>
             <Nav close={handleCloseSheet} containerStyles="flex flex-col items-center gap-y-6" linkStyles="text-2xl hover:text-primary dark:hover:text-darkMode" />
